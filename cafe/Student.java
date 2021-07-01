deec2b481cc5e875041128daede7cc2e6e2d2023
AlexanderMcLeod <mcleoal2@whs.school.nz>
2021-07-01 13:00:59 +1200

    Completed and annotated class

--- cafe/Student.java
+++ cafe/Student.java
@@ -1,3 +1,5 @@
 package cafe;
 
+// Inherits the Customer class without changing it at all.
+// Does not allow the user to set whether the customer can push in the line because they cannot
 public class Student extends Customer {}
