35b38621be075765c3007514f56c8dd6cec2bec9
AlexanderMcLeod <mcleoal2@whs.school.nz>
2021-07-01 17:16:55 +1200

    Updated app to print out median, mode, range and standard deviation which was added to statistics

--- cafe/App.java
+++ cafe/App.java
@@ -1,8 +1,8 @@
 package cafe;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class App {
 
@@ -11,6 +11,7 @@ public class App {
     Timeline timeline;
     Simulation sim;
     Scanner sc = new Scanner(System.in);
+    DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
     while (true) {
 
@@ -21,16 +22,44 @@ public class App {
         break;
       }
 
+      long startTime = System.nanoTime();
+
       timeline = FileAccessLayer.openFile(path); // Opens the file of the path the user inputted and transform it into a timeline data structure
       sim = new Simulation(timeline); // Creates a simulation using the timeline that was openend
       
       Statistics pushedIn = sim.getPushedInSimulation(); // Gets the statistics for the simulation
-      System.out.println("Mean student wait time in queue with staff pushing in: " + Double.valueOf(pushedIn.getMeanStudentWaitTime()));
-      System.out.println("Mean staff wait time in queue with staff pushing in: " + pushedIn.getMeanStaffWaitTime());
+      System.out.println("Mean student wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getMeanStudentWaitTime()));
+      System.out.println("Mean staff wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getMeanStaffWaitTime()));
+      System.out.println("Median student wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getMedianStudentWaitTime()));
+      System.out.println("Median staff wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getMedianStaffWaitTime()));
+      System.out.println("Mode student wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getModeStudentWaitTime()));
+      System.out.println("Mode staff wait time in queue with staff pushing in: " + decimalFormat.format(pushedIn.getModeStaffWaitTime()));
+      
+      System.out.println("The range of student wait times in queue with staff pushing in: " + decimalFormat.format(pushedIn.getRangeOfStudentWaitTimes()));
+      System.out.println("The range of staff wait times in queue with staff pushing in: " + decimalFormat.format(pushedIn.getRangeOfStaffWaitTimes()));
+      System.out.println("Standard deviation of student wait time with staff pushing in was: " + decimalFormat.format(pushedIn.getStudentStandardDeviation()));
+      System.out.println("Standard deviation of staff wait time was with staff pushing in was: " + decimalFormat.format(pushedIn.getStaffStandardDeviation()));
+      
+      
       Statistics notPushedIn = sim.getNotPushedInSimulation();
-      System.out.println("Mean student wait time in queue with staff not pushing in: " + notPushedIn.getMeanStudentWaitTime());
-      System.out.println("Mean staff wait time in queue with staff not pushing in: " + notPushedIn.getMeanStaffWaitTime());
-    
+      System.out.println("Mean student wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getMeanStudentWaitTime()));
+      System.out.println("Mean staff wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getMeanStaffWaitTime()));
+      System.out.println("Median student wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getMedianStudentWaitTime()));
+      System.out.println("Median staff wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getMedianStaffWaitTime()));
+      System.out.println("Mode student wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getModeStudentWaitTime()));
+      System.out.println("Mode staff wait time in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getModeStaffWaitTime()));
+      
+      System.out.println("The range of student wait times in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getRangeOfStudentWaitTimes()));
+      System.out.println("The range of staff wait times in queue with staff not pushing in: " + decimalFormat.format(notPushedIn.getRangeOfStaffWaitTimes()));
+      System.out.println("Standard deviation of student wait time with staff not pushing in was: " + decimalFormat.format(pushedIn.getStudentStandardDeviation()));
+      System.out.println("Standard deviation of staff wait time with staff not pushing in was: " + decimalFormat.format(pushedIn.getStaffStandardDeviation()));
+      
+      long endTime = System.nanoTime();
+
+      int convertToMillisecondsFromNanoSeconds = 1000000;
+      long duration = (endTime - startTime) / convertToMillisecondsFromNanoSeconds;
+
+      System.out.println("Calculated in " + Long.valueOf(duration) + "ms");
     }
 
     sc.close();
