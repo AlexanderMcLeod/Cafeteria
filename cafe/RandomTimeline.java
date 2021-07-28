e69016b0d74a528ad9228ddda8fed0d9f093df29
AlexanderMcLeod <mcleoal2@whs.school.nz>
2021-07-28 13:55:18 +1200

    Added more comments

--- cafe/RandomTimeline.java
+++ cafe/RandomTimeline.java
@@ -19,25 +19,25 @@ public class RandomTimeline {
     
     for (int minuteIndex = 0; minuteIndex < minuteCount; minuteIndex++) {
 
-      timeline.addNewMinute();
+      timeline.addNewMinute(); // Adds a new minute to the timeline
 
-      int randomStudentCount = new Random().nextInt(MAX_STUDENTS_PER_MINUTE);
-      timeline.getMinute(minuteIndex).setStudentsAddedToQueue(randomStudentCount);
-      personCount += randomStudentCount;
+      int randomStudentCount = new Random().nextInt(MAX_STUDENTS_PER_MINUTE); // Randomly chooses how many students are being added in that minute
+      timeline.getMinute(minuteIndex).setStudentsAddedToQueue(randomStudentCount); // Sets that random number in the timeline
+      personCount += randomStudentCount; // Updates the amount of people currently in the queue
 
-      int randomStaffCount = new Random().nextInt(MAX_STAFF_PER_MINUTE);
-      timeline.getMinute(minuteIndex).setStaffAddedToQueue(randomStaffCount);
-      personCount += randomStaffCount;
+      int randomStaffCount = new Random().nextInt(MAX_STAFF_PER_MINUTE); // Randomly chooses how many staff are being added to the queue in that minute
+      timeline.getMinute(minuteIndex).setStaffAddedToQueue(randomStaffCount); // Sets that random number in the timeline
+      personCount += randomStaffCount; // Updates the amount of people currently in the queue
 
-      int randomCustomersRemovedCount = new Random().nextInt(Math.min(MAX_CUSTOMERS_SERVED_PER_MINUTE, personCount));
-      timeline.getMinute(minuteIndex).setCustomersRemovedFromQueue(randomCustomersRemovedCount);
-      personCount -= randomCustomersRemovedCount;
+      int randomCustomersRemovedCount = new Random().nextInt(Math.min(MAX_CUSTOMERS_SERVED_PER_MINUTE, personCount)); // Randomly chooses how many people are being served in that minute
+      timeline.getMinute(minuteIndex).setCustomersRemovedFromQueue(randomCustomersRemovedCount); // Sets that random number in the timeline
+      personCount -= randomCustomersRemovedCount; // Updates the amount of people currently in the queue
 
     }
 
-    int minuteIndex = minuteCount - 1;
-    timeline.getMinute(minuteIndex).setCustomersRemovedFromQueue(personCount);
-    return timeline;
+    int minuteIndex = minuteCount - 1; 
+    timeline.getMinute(minuteIndex).setCustomersRemovedFromQueue(personCount); // If there are still people who have not been served, serve all of them
+    return timeline; 
     
   }
 
--- cafe/Simulation.java
+++ cafe/Simulation.java
@@ -11,22 +11,22 @@ public class Simulation {
 
   public Statistics getSimulation (final boolean STAFF_WILL_PUSH_IN) {
 
-    CafeQueue cafeQueue = new CafeQueue();
+    CafeQueue cafeQueue = new CafeQueue(); // Instantiate a new queue
 
-    for (int minuteIndex = 0; minuteIndex < timeline.size(); minuteIndex++){
+    for (int minuteIndex = 0; minuteIndex < timeline.size(); minuteIndex++){ // For every minute in the timeline
 
-      Timeline.Minute minute = timeline.getMinute(minuteIndex);
+      Timeline.Minute minute = timeline.getMinute(minuteIndex); // Get the current minute from the timeline
 
-      cafeQueue.addManyStudents(minuteIndex, minute.getStudentsAddedToQueue());
-      cafeQueue.addManyStaff(minuteIndex, STAFF_WILL_PUSH_IN, minute.getStaffAddedToQueue());
-      cafeQueue.removeManyFromFront(minuteIndex, minute.getCustomersRemovedFromQueue());
+      cafeQueue.addManyStudents(minuteIndex, minute.getStudentsAddedToQueue()); // Add the amount of students added in that minute to the queue
+      cafeQueue.addManyStaff(minuteIndex, STAFF_WILL_PUSH_IN, minute.getStaffAddedToQueue()); // Add the amount of staff added in that minute to the queue
+      cafeQueue.removeManyFromFront(minuteIndex, minute.getCustomersRemovedFromQueue()); // Remove the amount of customers served in that minute from the queue
 
     }
 
-    cafeQueue.getStatistics().setStudentQueueSize(Statistics.getStudentQueueSizeFromQueue(cafeQueue));
+    cafeQueue.getStatistics().setStudentQueueSize(Statistics.getStudentQueueSizeFromQueue(cafeQueue)); // Add the amount of people still in the queue to the statistics
     cafeQueue.getStatistics().setStaffQueueSize(Statistics.getStaffQueueSizeFromQueue(cafeQueue));
 
-    return cafeQueue.getStatistics();
+    return cafeQueue.getStatistics(); // Return the statistics from the simulation
 
   }
   
